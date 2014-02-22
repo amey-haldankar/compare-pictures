f0802fb34a7a21788945bb441f6f4a25e6104597
amey.haldankar07@gmail.com <amey.haldankar07@gmail.com@d301294e-1927-36ee-52a9-6a99a685d816>
2014-02-22 12:58:58 +0000

    #dailogs updated #constant theme for all dialogs #date 22/02/2014

--- CompareApp/src/org/lockerz/compare/PaintActivity.java
+++ CompareApp/src/org/lockerz/compare/PaintActivity.java
@@ -1,12 +1,9 @@
-
-
 package org.lockerz.compare;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.lockerz.compare.guideScreens.DemoActivity3;
-
 
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -52,21 +49,20 @@ ColorPickerDialog.OnColorChangedListener {
 	public int displayHeight;
 	public MyView mView;
 
-	//Bitmaps
-	public Bitmap mBitmap, mBackBitmap/*,bmOverlay,mutableBitmap*/;
+	// Bitmaps
+	public Bitmap mBitmap, mBackBitmap/* ,bmOverlay,mutableBitmap */;
 
-	//is saved
+	// is saved
 	public boolean isSaved = false;
 
 	public final String SAVE = "save";
 	public final String SHARE = "share";
-	public String  filePublic = "";
-	int check =-1;
-	public String shareSave ;
+	public String filePublic = "";
+	int check = -1;
+	public String shareSave;
 
 	@Override
-	protected void onCreate (Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		displayWidth = getWindowManager().getDefaultDisplay().getWidth();
@@ -91,17 +87,14 @@ ColorPickerDialog.OnColorChangedListener {
 
 		mBlur = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
 
-
-
 		firstRunPreferences();
-		if(getFirstRun(2))
-		{
-			Intent intentDemo = new Intent(PaintActivity.this,DemoActivity3.class);
+		if (getFirstRun(2)) {
+			Intent intentDemo = new Intent(PaintActivity.this,
+					DemoActivity3.class);
 			startActivity(intentDemo);
 			setRunned(2);
 		}
 	}
-
 
 	/**
 	 * 
@@ -112,19 +105,16 @@ ColorPickerDialog.OnColorChangedListener {
 	 * @detail
 	 */
 
-	 public void saveBitmap (View v)
-	{
+	public void saveBitmap(View v) {
 		mView.saveBitmaps(SAVE);
 	}
 
-	public void shareSnap (View v)
-	{
+	public void shareSnap(View v) {
 		mView.saveBitmaps(SHARE);
 	}
 
 	@Override
-	public void colorChanged (int color)
-	{
+	public void colorChanged(int color) {
 		mPaint.setColor(color);
 	}
 
@@ -139,8 +129,7 @@ ColorPickerDialog.OnColorChangedListener {
 		private float sX, sY;
 		private static final float TOUCH_TOLERANCE = 1;
 
-		public MyView (Context c)
-		{
+		public MyView(Context c) {
 			super(c);
 
 			BitmapFactory.Options options = new BitmapFactory.Options();
@@ -149,13 +138,17 @@ ColorPickerDialog.OnColorChangedListener {
 			options.inPreferredConfig = Config.ARGB_8888;
 			options.inJustDecodeBounds = false;
 
-			String pathExt = Environment.getExternalStorageDirectory().toString();
+			String pathExt = Environment.getExternalStorageDirectory()
+					.toString();
 			String path = pathExt + "/Pictures/MyCameraApp/snapImage.jpg";
 
-			/*mBackBitmap = BitmapFactory
-            .decodeFile("/mnt/sdcard/Pictures/MyCameraApp/snapImage.jpg",options);*/
+			/*
+			 * mBackBitmap = BitmapFactory
+			 * .decodeFile("/mnt/sdcard/Pictures/MyCameraApp/snapImage.jpg"
+			 * ,options);
+			 */
 
-			mBackBitmap = BitmapFactory.decodeFile(path,options);
+			mBackBitmap = BitmapFactory.decodeFile(path, options);
 
 			// mBackBitmap = Bitmap.createScaledBitmap(mBackBitmap,
 			// mBackBitmap.getWidth(), mBackBitmap.getHeight(), true);
@@ -164,72 +157,64 @@ ColorPickerDialog.OnColorChangedListener {
 		}
 
 		@Override
-		protected void onSizeChanged (int w, int h, int oldw, int oldh)
-		{
+		protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 			super.onSizeChanged(w, h, oldw, oldh);
 
-			/*mBitmap = Bitmap.createScaledBitmap(mBackBitmap, mBackBitmap
-                    .getWidth(), mBackBitmap.getHeight(), true);
-             mutableBitmap = mBitmap.copy(Bitmap.Config.ARGB_8888, true);
-
-            mCanvas = new Canvas(mutableBitmap);*/
+			/*
+			 * mBitmap = Bitmap.createScaledBitmap(mBackBitmap, mBackBitmap
+			 * .getWidth(), mBackBitmap.getHeight(), true); mutableBitmap =
+			 * mBitmap.copy(Bitmap.Config.ARGB_8888, true);
+			 * 
+			 * mCanvas = new Canvas(mutableBitmap);
+			 */
 			mBitmap = mBackBitmap.copy(mBackBitmap.getConfig(), true);
 			mCanvas = new Canvas(mBitmap);
 		}
 
 		@Override
-		protected void onDraw (Canvas canvas)
-		{
+		protected void onDraw(Canvas canvas) {
 			canvas.drawColor(0x00FFFFFF);
-			//canvas.drawBitmap(mBackBitmap, 0, 0, null);
+			// canvas.drawBitmap(mBackBitmap, 0, 0, null);
 			canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
 
 			canvas.drawPath(mPath, mPaint);
 		}
 
-
-		public void Reset ( )
-		{ 
+		public void Reset() {
 			mPath.reset();
-			mBitmap  =mBackBitmap.copy(mBackBitmap.getConfig(), true);
+			mBitmap = mBackBitmap.copy(mBackBitmap.getConfig(), true);
 			mCanvas = new Canvas(mBitmap);
 			invalidate();
 		}
 
+		/*
+		 * public Bitmap overlay (Bitmap bmp1, Bitmap bmp2) { bmOverlay =
+		 * Bitmap.createBitmap(bmp1.getWidth(), bmp1 .getHeight(),
+		 * bmp1.getConfig());
+		 * 
+		 * Canvas canvas = new Canvas(bmOverlay); canvas.drawBitmap(bmp1, new
+		 * Matrix(), null); canvas.drawBitmap(bmp2, 0, 0, null); return
+		 * bmOverlay; }
+		 */
 
-		/*public Bitmap overlay (Bitmap bmp1, Bitmap bmp2)
-		{
-			bmOverlay = Bitmap.createBitmap(bmp1.getWidth(), bmp1
-					.getHeight(), bmp1.getConfig());
-
-			Canvas canvas = new Canvas(bmOverlay);
-			canvas.drawBitmap(bmp1, new Matrix(), null);
-			canvas.drawBitmap(bmp2, 0, 0, null);
-			return bmOverlay;
-		}*/
-
-		private void touchStart (float x, float y)
-		{
+		private void touchStart(float x, float y) {
 			mPath.reset();
 			mPath.moveTo(x, y);
 			sX = x;
 			sY = y;
 		}
 
-		private void touchMove (float x, float y)
-		{
+		private void touchMove(float x, float y) {
 			float dx = Math.abs(x - sX);
 			float dy = Math.abs(y - sY);
-			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE)
-			{
+			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
 				mPath.quadTo(sX, sY, (x + sX) / 2, (y + sY) / 2);
 				sX = x;
 				sY = y;
 			}
 		}
 
-		private void touchUp ( )
-		{
+		private void touchUp() {
 			mPath.lineTo(sX, sY);
 			// commit the path to our offscreen
 			mCanvas.drawPath(mPath, mPaint);
@@ -238,22 +223,20 @@ ColorPickerDialog.OnColorChangedListener {
 		}
 
 		@Override
-		public boolean onTouchEvent (MotionEvent event)
-		{
+		public boolean onTouchEvent(MotionEvent event) {
 			float x = event.getX();
 			float y = event.getY();
 
-			switch (event.getAction())
-			{
-			case MotionEvent.ACTION_DOWN :
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
 				touchStart(x, y);
 				invalidate();
 				break;
-			case MotionEvent.ACTION_MOVE :
+			case MotionEvent.ACTION_MOVE:
 				touchMove(x, y);
 				invalidate();
 				break;
-			case MotionEvent.ACTION_UP :
+			case MotionEvent.ACTION_UP:
 				touchUp();
 				invalidate();
 				break;
@@ -261,42 +244,31 @@ ColorPickerDialog.OnColorChangedListener {
 			return true;
 		}
 
-
-
-		public void saveBitmaps (String option)
-		{  
-			if(option=="0")
-			{
+		public void saveBitmaps(String option) {
+			if (option == "0") {
 				new SaveBitmapped().execute(null);
 				dismissDialog(0);
-			}
-			else if(option!="1" && option!="0")
-			{
-				shareSave =option;
+			} else if (option != "1" && option != "0") {
+				shareSave = option;
 
 				showDialog(0);
-			}
-			else
-			{
+			} else {
 				dismissDialog(0);
 			}
 
 		}
 
-
 		/**
 		 * 
 		 * @brief
-		 *
+		 * 
 		 * @detail Async task for creating file and saving the bitmap
 		 */
-		class SaveBitmapped extends AsyncTask<Void, Void, Void>
-		{
+		class SaveBitmapped extends AsyncTask<Void, Void, Void> {
 			ProgressDialog pd;
 
 			@Override
-			protected void onPreExecute ( )
-			{
+			protected void onPreExecute() {
 				pd = new ProgressDialog(PaintActivity.this);
 				pd.setTitle(R.string.save_img_text);
 				pd.setMessage("Saving...");
@@ -304,10 +276,9 @@ ColorPickerDialog.OnColorChangedListener {
 			}
 
 			@Override
-			protected Void doInBackground (Void... params)
-			{
+			protected Void doInBackground(Void... params) {
 				try {
-					//mBackBitmap = overlay(mBackBitmap, mBitmap);
+					// mBackBitmap = overlay(mBackBitmap, mBitmap);
 					FileOutput fo = new FileOutput();
 					fo.writeToFile(filePublic, mBitmap);
 				} catch (Exception e) {
@@ -315,34 +286,35 @@ ColorPickerDialog.OnColorChangedListener {
 					e.printStackTrace();
 				}
 
-
 				return null;
 			}
 
 			@Override
-			protected void onPostExecute (Void result)
-			{
+			protected void onPostExecute(Void result) {
 				isSaved = true;
 				pd.dismiss();
-				if(shareSave == SAVE)
-				{
+				if (shareSave == SAVE) {
 
-				}
-				else if(shareSave == SHARE)
-				{
-					String pathExt = Environment.getExternalStorageDirectory().toString();
-					String path = pathExt + "/Pictures/MyCameraApp/" + filePublic + ".jpg";
+				} else if (shareSave == SHARE) {
+					String pathExt = Environment.getExternalStorageDirectory()
+							.toString();
+					String path = pathExt + "/Pictures/MyCameraApp/"
+							+ filePublic + ".jpg";
 
 					Intent shareIntent = new Intent(Intent.ACTION_SEND);
 					shareIntent.setType("image/*");
 
-					/*shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"
-                            + "/mnt/sdcard/Pictures/MyCameraApp/editedImage.jpg"));*/
+					/*
+					 * shareIntent.putExtra(Intent.EXTRA_STREAM,
+					 * Uri.parse("file://" +
+					 * "/mnt/sdcard/Pictures/MyCameraApp/editedImage.jpg"));
+					 */
 
-					shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"
-							+ path));
+					shareIntent.putExtra(Intent.EXTRA_STREAM,
+							Uri.parse("file://" + path));
 
-					startActivity(Intent.createChooser(shareIntent, "Share image using"));
+					startActivity(Intent.createChooser(shareIntent,
+							"Share image using"));
 				}
 			}
 
@@ -353,164 +325,177 @@ ColorPickerDialog.OnColorChangedListener {
 	private static final int EMBOSS_MENU_ID = Menu.FIRST + 1;
 	private static final int BLUR_MENU_ID = Menu.FIRST + 2;
 	private static final int RESET_MENU_ID = Menu.FIRST + 3;
-	//private static final int ERASE_MENU_ID = Menu.FIRST + 3;
+	// private static final int ERASE_MENU_ID = Menu.FIRST + 3;
 	private static final int SRCATOP_MENU_ID = Menu.FIRST + 4;
 
 	@Override
-	public boolean onCreateOptionsMenu (Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 
-		menu.add(0, COLOR_MENU_ID, 0, R.string.menu_color).setShortcut('3', 'c');
-		menu.add(0, EMBOSS_MENU_ID, 0, R.string.menu_emboss).setShortcut('4', 's');
+		menu.add(0, COLOR_MENU_ID, 0, R.string.menu_color)
+		.setShortcut('3', 'c');
+		menu.add(0, EMBOSS_MENU_ID, 0, R.string.menu_emboss).setShortcut('4',
+				's');
 		menu.add(0, BLUR_MENU_ID, 0, R.string.menu_blur).setShortcut('5', 'z');
-		menu.add(0, RESET_MENU_ID, 0, R.string.menu_reset).setShortcut('5', 'z');
-		//menu.add(0, ERASE_MENU_ID, 0, "Erase").setShortcut('5', 'z');
+		menu.add(0, RESET_MENU_ID, 0, R.string.menu_reset)
+		.setShortcut('5', 'z');
+		// menu.add(0, ERASE_MENU_ID, 0, "Erase").setShortcut('5', 'z');
 		// menu.add(0, SRCATOP_MENU_ID, 0, "SrcATop").setShortcut('5', 'z');
 
 		return true;
 	}
 
 	@Override
-	public boolean onPrepareOptionsMenu (Menu menu)
-	{
+	public boolean onPrepareOptionsMenu(Menu menu) {
 		super.onPrepareOptionsMenu(menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected (MenuItem item)
-	{
+	public boolean onOptionsItemSelected(MenuItem item) {
 		mPaint.setXfermode(null);
 		mPaint.setAlpha(0xFF);
 
-		switch (item.getItemId())
-		{
-		case COLOR_MENU_ID :
+		switch (item.getItemId()) {
+		case COLOR_MENU_ID:
 			new ColorPickerDialog(this, this, mPaint.getColor()).show();
 			return true;
-		case EMBOSS_MENU_ID :
-			if (mPaint.getMaskFilter() != mEmboss)
-			{
+		case EMBOSS_MENU_ID:
+			if (mPaint.getMaskFilter() != mEmboss) {
 				mPaint.setMaskFilter(mEmboss);
-			} else
-			{
+			} else {
 				mPaint.setMaskFilter(null);
 			}
 			return true;
-		case BLUR_MENU_ID :
-			if (mPaint.getMaskFilter() != mBlur)
-			{
+		case BLUR_MENU_ID:
+			if (mPaint.getMaskFilter() != mBlur) {
 				mPaint.setMaskFilter(mBlur);
-			} else
-			{
+			} else {
 				mPaint.setMaskFilter(null);
 			}
 			return true;
-			/*case ERASE_MENU_ID :
-                mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-                return true;*/
-			case RESET_MENU_ID :
-				mView.Reset();
-				return true;
+			/*
+			 * case ERASE_MENU_ID : mPaint.setXfermode(new
+			 * PorterDuffXfermode(PorterDuff.Mode.CLEAR)); return true;
+			 */
+		case RESET_MENU_ID:
+			mView.Reset();
+			return true;
 
-			case SRCATOP_MENU_ID :
-				mPaint.setXfermode(new PorterDuffXfermode(
-						PorterDuff.Mode.SRC_ATOP));
-				mPaint.setAlpha(0x80);
-				return true;
+		case SRCATOP_MENU_ID:
+			mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+			mPaint.setAlpha(0x80);
+			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
-	protected void onDestroy ( )
-	{
+	protected void onDestroy() {
 		super.onDestroy();
-		if(mBackBitmap != null)
-		{
+		if (mBackBitmap != null) {
 			mBackBitmap.recycle();
 			mBackBitmap = null;
 		}
-		if(mBitmap != null)
-		{
+		if (mBitmap != null) {
 			mBitmap.recycle();
 			mBitmap = null;
 		}
-		/*if(bmOverlay != null)
-		{
-			bmOverlay.recycle();
-			bmOverlay = null;
-		}
-		if(mutableBitmap !=null)
-		{
-			mutableBitmap.recycle();
-			mutableBitmap= null ; 
-		}*/
+		/*
+		 * if(bmOverlay != null) { bmOverlay.recycle(); bmOverlay = null; }
+		 * if(mutableBitmap !=null) { mutableBitmap.recycle(); mutableBitmap=
+		 * null ; }
+		 */
 
 	}
 
-
 	@Override
-	public void onBackPressed ( )
-	{
+	public void onBackPressed() {
 
-		AlertDialog.Builder alertbox = new AlertDialog.Builder(PaintActivity.this);
+		AlertDialog.Builder alertbox = new AlertDialog.Builder(
+				PaintActivity.this);
+		//
+		View view = View.inflate(PaintActivity.this,R.layout.alert_box_layout, null);
 
-		alertbox.setMessage(R.string.unsaved_changes_text); // Message to be displayed
+		alertbox.setView(view);
 
-		alertbox.setPositiveButton(R.string.continue_btn,new DialogInterface.OnClickListener() {
-
+		alertbox.setMessage(R.string.unsaved_changes_text); // Message to be displayed.
+		
+		Button continueBtn = (Button)view.findViewById(R.id.contBtn);
+		Button cancelBtn = (Button)view.findViewById(R.id.cancelBtn);
+		
+		continueBtn.setOnClickListener(new OnClickListener() {
+			
 			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-
+			public void onClick(View v) {
+			
 				PaintActivity.super.onBackPressed();
 			}
-
 		});
-
-		alertbox.setNegativeButton(R.string.cancel_btn, new DialogInterface.OnClickListener() {
-
+		
+		cancelBtn.setOnClickListener(new OnClickListener() {
+			
 			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-
+			public void onClick(View v) {
+				
+				finish();
 			}
-
 		});
+
+		/*alertbox.setPositiveButton(R.string.continue_btn,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+
+						PaintActivity.super.onBackPressed();
+					}
+
+				});
+
+		alertbox.setNegativeButton(R.string.cancel_btn,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+
+					}
+
+				});*/
 
 		// show the alert box will be swapped by other code later
 		alertbox.show();
 
 	}
 
-
-	EditText filEdit ;
+	EditText filEdit;
 	Button yesButton;
 	Button noButton;
 
 	protected Dialog onCreateDialog(int d) {
 
+		Dialog dialog = new Dialog(PaintActivity.this);
 
-		final Dialog dialog  = new Dialog(PaintActivity.this);
 		dialog.setContentView(R.layout.save_image);
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
 		dialog.setTitle(R.string.save_img_as_text);
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+		.format(new Date());
+
+		// dialog.setTitle(R.string.save_img_as_text);
 		filEdit = (EditText) dialog.findViewById(R.id.editText1);
 		yesButton = (Button) dialog.findViewById(R.id.buttonYes);
 		noButton = (Button) dialog.findViewById(R.id.buttonNo);
 
 		filEdit.setText(timeStamp);
 
-
 		yesButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				filePublic=filEdit.getText().toString().trim();
+				filePublic = filEdit.getText().toString().trim();
 				new MyView(getApplicationContext()).saveBitmaps("0");
 			}
 
@@ -524,14 +509,8 @@ ColorPickerDialog.OnColorChangedListener {
 			}
 		});
 
-
-
-
 		return dialog;
-
 
 	}
 
-
 }
-
